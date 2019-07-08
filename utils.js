00139e9986a5feca56bba1d425165f139c5b1da6
fengchen <xie_cang@163.com>
2019-07-08 08:57:03 +0800

    提取公共函数到 utils

---
+++ utils.js
@@ -0,0 +1,20 @@
+var log = console.log.bind(console)
+
+// 加载图片
+var imageFromPath = function (path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+// 判断相撞的函数
+var rectIntersects = function (a, b) {
+    var o = a
+    if (b.y > o.y && b.y < o.y + o.image.height) {
+        if (b.x > o.x && b.x < o.x + o.image.width) {
+            log('相撞了')
+            return true
+        }
+    }
+    return false
+}
